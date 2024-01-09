43ebe67c8f0a6850ae0a5b9037d26844d67f0b9e
Romaric Zounlome <rzounlom@gmail.com>
2024-01-09 13:19:23 -0500

    chore: update spinner

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -12,22 +12,24 @@ type TodoListProps = {
 const TodoList: FC<TodoListProps> = ({ todos, activeTodoCount, loading }) => {
   if (loading) {
     return (
-      <div className="min-h-[368px] w-full flex justify-center items-center mt-[16px] bg-[#E3E4F1] dark:bg-[#393A4B] rounded">
+      <div className="h-[368px] md:h-[540px] w-full flex justify-center items-center mt-[16px] bg-[#E3E4F1] dark:bg-[#393A4B] rounded">
         <Spinner />
       </div>
     );
   }
 
   return (
-    <div className="max-h-[368px] h-[auto] w-full mt-[16px] bg-[#E3E4F1] dark:bg-[#393A4B] rounded overflow-auto shadow-2xl">
-      {todos.map((todo: Todo) => (
-        <TodoRow todo={todo} key={todo.id} />
-      ))}
-      <div className="w-full h-[48px] flex justify-between items-center rounded bg-[#FFF] dark:bg-[#25273D] text-[#9495A5] px-6 mt-[1px] shadow-md md:hidden">
+    <>
+      <div className="h-[368px] md:h-[492px] max-h-[540px] w-full mt-[16px] bg-[#E3E4F1] dark:bg-[#393A4B] rounded overflow-auto shadow-2xl relative">
+        {todos.length
+          ? todos.map((todo: Todo) => <TodoRow todo={todo} key={todo.id} />)
+          : "No todos"}
+      </div>
+      <div className="w-[327px] md:w-[540px] h-[48px] flex justify-between items-center rounded bg-[#FFF] dark:bg-[#25273D] text-[#9495A5] px-6 mt-[1px] shadow-md md:hidden absolute">
         <div>{activeTodoCount} items left</div>
         <div className="hover:cursor-pointer">Clear Completed</div>
       </div>
-    </div>
+    </>
   );
 };
 
