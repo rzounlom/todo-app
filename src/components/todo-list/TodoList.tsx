4446b9e31ac9a1d58dc994532a0a5a8f5f2a62c6
Romaric Zounlome <rzounlom@gmail.com>
2024-01-09 05:20:46 -0500

    feat: add mobile styles

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -4,6 +4,7 @@ import TodoRow from "../todo-card/TodoRow";
 
 type TodoListProps = {
   todos: Todo[];
+  activeTodoCount: number;
 };
 
 const todo: Todo = {
@@ -12,12 +13,16 @@ const todo: Todo = {
   completed: true,
 };
 
-const TodoList: FC<TodoListProps> = ({ todos }) => {
+const TodoList: FC<TodoListProps> = ({ todos, activeTodoCount }) => {
   return (
-    <div className="max-h-[368px] h-[auto] w-full mt-[16px] bg-[#E3E4F1] rounded overflow-auto shadow-2xl">
+    <div className="max-h-[368px] h-[auto] w-full mt-[16px] bg-[#E3E4F1] dark:bg-[#393A4B] rounded overflow-auto shadow-2xl">
       {todos.map((todo: Todo) => (
         <TodoRow todo={todo} key={todo.id} />
       ))}
+      <div className="w-full h-[48px] flex justify-between items-center rounded bg-[#FFF] dark:bg-[#25273D] text-[#9495A5] px-6 mt-[1px] shadow-md md:hidden">
+        <div>{activeTodoCount} items left</div>
+        <div className="hover:cursor-pointer">Clear Completed</div>
+      </div>
     </div>
   );
 };
