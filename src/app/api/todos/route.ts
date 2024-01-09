e276a34a227a922d714c5e2f0a6f7cfd216c5215
Romaric Zounlome <rzounlom@gmail.com>
2024-01-09 11:35:28 -0500

    feat: add new routs for create/get todos

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/utils/db";
+
+export const GET = async (req: Request, res: Response) => {
+  const todos = await prisma.todo.findMany({});
+
+  return NextResponse.json({ data: todos });
+};
+
+export const POST = async (req: Request, res: Response) => {
+  if (!req.body) {
+    return NextResponse.next();
+  }
+
+  const { todo } = await req.json(); // Extract the text from the request body
+
+  const newTodo = await prisma.todo.create({
+    data: {
+      text: todo.text, // Use the extracted text from the request body
+      completed: false,
+    },
+  });
+
+  return NextResponse.json({ data: newTodo });
+};
