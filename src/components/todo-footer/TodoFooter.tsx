d11bd87cb4369abb340fcd1d0f4e62d9cfef5891
Romaric Zounlome <rzounlom@gmail.com>
2024-01-09 13:19:10 -0500

    chore: refactor loading

--- src/components/todo-footer/TodoFooter.tsx
+++ src/components/todo-footer/TodoFooter.tsx
@@ -16,30 +16,31 @@ const TodoFooter: FC<TodoFooterProps> = ({
   todoLength,
   loading,
 }) => {
-  if (loading) {
-    return null;
-  }
-
   return (
-    <div className="w-full h-[48px] flex justify-center items-center rounded bg-[#FFF] dark:bg-[#25273D] text-[#9495A5] font-bold px-6 mt-[16px] md:mt-[1px] shadow-md">
+    <div className="w-full h-[48px] flex justify-center items-center rounded bg-[#FFF] dark:bg-[#25273D] text-[#9495A5] font-bold px-6 mt-[52px] md:mt-[1px] shadow-md">
       <div className="w-full h-full flex justify-center md:justify-between items-center">
-        <div className="hidden md:block">{todoLength} items left</div>
-        <div className="w-[166px] flex justify-between items-center">
-          {tabs.map((tab: Tab) => (
-            <div
-              key={tab.id}
-              className={`hover:cursor-pointer ${
-                tab.name === activeTab ? "text-[#3A7CFD]" : ""
-              } $}`}
-              onClick={() => setActiveTab(tab.name)}
-            >
-              {tab.label}
+        {loading ? null : (
+          <>
+            {" "}
+            <div className="hidden md:block">{todoLength} items left</div>
+            <div className="w-[166px] flex justify-between items-center">
+              {tabs.map((tab: Tab) => (
+                <div
+                  key={tab.id}
+                  className={`hover:cursor-pointer ${
+                    tab.name === activeTab ? "text-[#3A7CFD]" : ""
+                  } $}`}
+                  onClick={() => setActiveTab(tab.name)}
+                >
+                  {tab.label}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="hover:cursor-pointer hidden md:block">
-          Clear Completed
-        </div>
+            <div className="hover:cursor-pointer hidden md:block">
+              Clear Completed
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
