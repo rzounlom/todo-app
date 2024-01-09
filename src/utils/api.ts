ea9fa854c73fb3296acd53990a6add1433ae988e
Romaric Zounlome <rzounlom@gmail.com>
2024-01-09 11:36:02 -0500

    feat: create new API routs to get/create todos

---
+++ src/utils/api.ts
@@ -0,0 +1,32 @@
+import { Todo } from "@/types/todo";
+
+const createURL = (path: string) => window.location.origin + path;
+
+export const getTodos = async () => {
+  const res = await fetch(
+    new Request(createURL(`/api/todos`), {
+      method: "GET",
+    })
+  );
+
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Something went wrong on API server!");
+  }
+};
+
+export const addTodo = async (todo: Partial<Todo>) => {
+  const res = await fetch(
+    new Request(createURL(`/api/todos`), {
+      method: "POST",
+      body: JSON.stringify({ todo }),
+    })
+  );
+
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Something went wrong on API server!");
+  }
+};
