bca61d4d9d8c666c240f0b87a48346df38086876
Romaric Zounlome <rzounlom@gmail.com>
2024-01-09 13:24:04 -0500

    chore: update todos message

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -21,9 +21,13 @@ const TodoList: FC<TodoListProps> = ({ todos, activeTodoCount, loading }) => {
   return (
     <>
       <div className="h-[368px] md:h-[492px] max-h-[540px] w-full mt-[16px] bg-[#E3E4F1] dark:bg-[#393A4B] rounded overflow-auto shadow-2xl relative">
-        {todos.length
-          ? todos.map((todo: Todo) => <TodoRow todo={todo} key={todo.id} />)
-          : "No todos"}
+        {todos.length ? (
+          todos.map((todo: Todo) => <TodoRow todo={todo} key={todo.id} />)
+        ) : (
+          <div className="h-full w-full flex justify-center items-center text-[30px] md:text-[40px]">
+            No todos!
+          </div>
+        )}
       </div>
       <div className="w-[327px] md:w-[540px] h-[48px] flex justify-between items-center rounded bg-[#FFF] dark:bg-[#25273D] text-[#9495A5] px-6 mt-[1px] shadow-md md:hidden absolute">
         <div>{activeTodoCount} items left</div>
